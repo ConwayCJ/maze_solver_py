0963c622016696ce0af5c7f9a32cf9197cf51176
ConwayCJ <christopherjconway07@gmail.com>
2024-05-19 20:59:08 -0700

    Add cell drawing functionality!

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from graphics import Window, Line, Point
+from graphics import Window, Line, Point, Cell
+
+# 
 
 def main():
   print("Creating new window")
   win = Window(800,600)
 
   win.draw_line(Line(Point(50,50), Point(400,400)), "black")
-  
+  cell = Cell(win,100,100,500,500)
+  cell.draw()
   win.wait_for_close()
 
 main()
